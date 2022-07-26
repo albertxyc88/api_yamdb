01eab3af73d2075d3e5ab9d9e075a5328368e70e
Albert Husnadinov <albertxyc@yandex.ru>
2022-07-26 11:50:09 +0300

    Changing modeles.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -81,24 +81,3 @@ class Title(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class TitleGenre(models.Model):
-    title = models.ForeignKey(
-        Title,
-        verbose_name='Произведение',
-        on_delete=models.CASCADE
-    )
-    genre = models.ForeignKey(
-        Genre,
-        verbose_name='Жанр',
-        on_delete=models.CASCADE
-    )
-
-    class Meta:
-        verbose_name = 'Произведение и жанр'
-        verbose_name_plural = 'Произведения и жанры'
-        ordering = ('title',)
-
-    def __str__(self):
-        return f'{self.title} - {self.genre}'
