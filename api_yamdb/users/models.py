c8cd9b37b100e2c86effe56e823c50e74f2263ee
Альберт Хуснадинов <alberthusnadinov@MacBook-Air-Albert.local>
2022-08-04 13:25:08 +0300

    Fix second review errors.

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -20,11 +20,11 @@ class User(AbstractUser):
 
     @property
     def is_moderator(self):
-        return self.role == self.MODERATOR
+        return self.role == (self.MODERATOR or self.ADMIN or self.is_superuser)
 
     @property
     def is_admin(self):
-        return self.role == self.ADMIN
+        return self.role == self.ADMIN or self.is_superuser
 
     @property
     def is_user(self):
